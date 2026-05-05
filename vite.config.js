91364dd35503cede093be2dde7e0739d46a54835
Jared092005 <eliseoborja26@gmail.com>
2026-05-05 17:52:12 -0300

    agregué las otras paginas al vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolveConfig(__dirname, "index.html"),
+        about: resolveConfig(__dirname, "about.html"),
+        portafolio: resolveConfig(__dirname, "portafolio.html"),
+      },
+    },
+  },
 });
